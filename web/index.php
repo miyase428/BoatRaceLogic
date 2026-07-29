e8e1e56badf915dd4a7800b8589481d13a426c6c
miyase428 <miyase428@gmail.com>
2026-07-29 13:20:14 +0900

    fix: index_view.phpの読み込みを追加

--- web/index.php
+++ web/index.php
@@ -5,3 +5,5 @@ $controller = new IndexController();
 $viewData   = $controller->handle();
 
 extract($viewData); // $selected_date, $selected_place, $race_code などを展開
+
+include __DIR__ . '/views/index_view.php';
