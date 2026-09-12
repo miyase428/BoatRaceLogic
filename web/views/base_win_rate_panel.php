1eb465fe2ee0e05709ba13a47391d249bc2dd749
miyase428 <miyase428@gmail.com>
2026-09-13 07:35:42 +0900

    Load merged head1 second-place AI rows

--- web/views/base_win_rate_panel.php
+++ web/views/base_win_rate_panel.php
@@ -28,6 +28,7 @@ include __DIR__ . '/recent_prediction_history_panel.php';
 <link rel="stylesheet" href="/web/assets/css/pc_main_tabs.css?v=20260912a">
 <script src="/web/assets/js/pc_main_tabs.js?v=20260912a"></script>
 <script src="/web/assets/js/pc_rate_development_tab.js?v=20260913c"></script>
+<script src="/web/assets/js/pc_head1_second_merge.js?v=20260913a"></script>
 <script src="/web/assets/js/pc_exacta_tab.js?v=20260901a"></script>
 <script src="/web/assets/js/pc_bet_simulator_v3.js?v=20260901a"></script>
 <script src="/web/assets/js/live_trifecta_top2_strategy.js?v=20260901b"></script>
