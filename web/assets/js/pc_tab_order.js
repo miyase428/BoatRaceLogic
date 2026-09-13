e47be9114d22f2d7ed7dd4158bd33a0bd3e36e76
miyase428 <miyase428@gmail.com>
2026-09-13 20:06:44 +0900

    Bump current meet display asset version

--- web/assets/js/pc_tab_order.js
+++ web/assets/js/pc_tab_order.js
@@ -24,7 +24,7 @@
     function loadCurrentMeetScript() {
         if (document.querySelector('script[data-pc-current-meet-loader="1"]')) return;
         const script = document.createElement('script');
-        script.src = '/web/assets/js/pc_current_meet.js?v=20260913c';
+        script.src = '/web/assets/js/pc_current_meet.js?v=20260913d';
         script.dataset.pcCurrentMeetLoader = '1';
         script.async = false;
         document.head.appendChild(script);
